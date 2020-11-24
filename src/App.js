2a586765a4b5fe971f4b9e55ede5e40c8a7bef08
Cristian <cristian.valenciaa@alumnos.uv.cl>
2020-11-24 08:42:17 -0300

    Agregando primera parte de router

--- src/App.js
+++ src/App.js
@@ -5,33 +5,80 @@ import {ComponenteMenu} from './ComponenteMenu';
 import Validador from './ComponentesValidador/Validador';
 import { Formulario } from './Formulario';
 
+import Nav from './Nav';
+
+//Seccion de router
+import {BrowserRouter as 
+  Router, 
+  Switch, 
+  Route } from 'react-router-dom';
+
+
+
 function App() {
   return (
-    <div className="App">
-      <Header as='h2' icon textAlign='center'>     
-      <h1>Semantic UI</h1>
-        <Image
-          centered
-          size='large'
-          src='https://miro.medium.com/max/256/0*90ubl17sSaFcwNRu.png'
-        />
-      <h3>Para la clase de Desarrollo Web</h3>
-    </Header>
-    <Divider/>
-      <ComponenteMenu/>
-    <Divider/>
-      <Listado/>
-    <Divider/>
-      <Container textAlign="center">
-            <Validador valor=""/>
-      </Container>
-    <Divider/>
-      <Container>
-          <Formulario />
-      </Container>
+    <Router>
+      <div className="App">
+          <Header as='h2' icon textAlign='center'>
+            <h1>Semantic UI</h1>
+            <Image
+              centered
+              size='large'
+              src='https://miro.medium.com/max/256/0*90ubl17sSaFcwNRu.png'
+            />
+            <h3>Para la clase de Desarrollo Web</h3>
+            <div className="App-header1">
 
-    </div>
+            </div>
+          </Header>
+        <body>
+          <segment>
+            <div>
+              <div>
+                <div>
+                  <switch>
+                    <Route path="/" component={Home} />
+                    <Route path="/Menu" component={ComponenteMenu} />
+                    <Route path="/Listado" component={Listado} />
+                    <Route path="/Formulario" component={Formulario} />
+                    <Route path="/Validador" component={Validador} />
+                  </switch>
+                </div>
+              </div>
+            </div>
+          </segment>
+        </body>
+
+
+      </div>
+    </Router>
+
   );
 }
 
+/*
+<Divider />
+<ComponenteMenu />
+<Divider />
+<Listado />
+<Divider />
+<Container textAlign="center">
+  <Validador valor="" />
+</Container>
+<Divider />
+<Container>
+  <Formulario />
+</Container>
+*/
+
+const Home = () => (
+  <div>
+    <h1>
+      Pagina de inicio: "HOME"
+    </h1>
+    <div>
+      <Nav/>
+    </div>
+  </div>
+)
 export default App;
---
+++ src/Nav.js
@@ -0,0 +1,63 @@
+import React from "react";
+import './App.css';
+import { Link } from 'react-router-dom';
+
+function Nav() {
+
+    const navStyle = {
+        color: 'Black'
+    };
+    return (
+        <nav>
+            <ul className="nav-links">
+                <Link style={navStyle} to='/'>
+                    <li>
+                        Home
+                    </li>
+
+                </Link>
+                
+            </ul>
+            <ul className="nav-links">
+                <Link style={navStyle} to='/Menu'>
+                    <li>
+                        Menu
+                    </li>
+
+                </Link>
+                
+            </ul> 
+            <ul className="nav-links">
+                <Link style={navStyle} to='/Listado'>
+                    <li>
+                        Listado
+                    </li>
+
+                </Link>
+                
+            </ul> 
+            <ul className="nav-links">
+                <Link style={navStyle} to='/Formulario'>
+                    <li>
+                        Formulario
+                    </li>
+
+                </Link>
+                
+            </ul> 
+            <ul className="nav-links">
+                <Link style={navStyle} to='/Validador'>
+                    <li>
+                        Validador
+                    </li>
+
+                </Link>
+                
+            </ul>   
+        </nav>
+
+    );
+  }
+  
+  
+  export default Nav;
